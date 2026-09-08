4bf6c17f247a902aebb9c8b6bf29a15c5afade25
Aringgrh <Aaahary14@gmail.com>
2026-09-08 14:55:19 +0700

    feat(firebase): konfigurasi firebase dan inisialisasi pada aplikasi

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:fodos/firebase_options.dart';
 import 'package:fodos/service/preferencehandler.dart';
 import 'package:fodos/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await PreferenceHandler.init();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
